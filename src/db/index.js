2212babd270e0b69484136e382d64f9c27208f0b
Priyansh Namdeo <priyanshnamdeo7@gmail.com>
2025-12-02 17:19:27 +0530

    Connect to mongodb

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constraints.js";
+
+
+const connectDB = async () => {
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log(`\n MongoDB connected successfully to host: ${connectionInstance.connection.host}`);
+       
+
+    }catch(error){
+        console.log("Error in connecting to the mongoose", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,50 @@
+//require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+const app = express();
+
+
+;(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("Error in connecting to the server", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    }catch(error){
+        console.log("Error connecting to MongoDB", error);
+        throw error
+
+    }
+})()
+
+*/
